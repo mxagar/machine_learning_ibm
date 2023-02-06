4f939052e4d31c9aeccaad593d6a83851f754065
Mikel <mxagar@gmail.com>
2023-02-06 13:32:39 +0100

    app template

---
+++ 06_Capstone_Project/lab/first_app/app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+
+#1. Create a streamlit title widget, this will be shown first
+
+#2. Then create a streamlit title widget, this will be shown after title
+
+
+
+#3. Create two streamlit slider widget for receiving a numerical value input
+
+
+#4. Create a streamlit text widget to show the sum result of two slider values
+# Create a streamlit subheader widget
